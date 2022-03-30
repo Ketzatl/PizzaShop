62d1946b53347d4c84907b764953d02eb6e47f3c
Morgan <morgan.nott@hotmail.com>
2022-03-30 21:38:31 +0200

    - Création d'un fichier style.css et mise en place d'un début de style CSS - Création du template pour la page menu - Ajout d'une image en background de la page menu et d'un logo

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
+
 
 # Create your views here.
+
+def index(request):
+    # pizzas = Pizza.objects.all()
+    # pizzas_list_names_and_prices = [pizza.nom + ' : ' + str(pizza.prix) + '€' for pizza in pizzas]
+    # pizzas_names_list_str_prices = ', '.join(pizzas_list_names_and_prices)
+    # return HttpResponse("Les Pizzas disponibles : " + pizzas_names_list_str_prices)
+    pizzas = Pizza.objects.all()
+    return render(request, 'menu/index.html', {'pizzas': pizzas})
